01777eacf4e189ab751982aba3b9b19f798c8c3e
Marcos Seghesio <marcos.seghesio@gmail.com>
2020-07-08 21:14:03 -0300

    commit realizado

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,6 @@
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
-function mostrar()
-{
-	alert("Ok");
+function mostrar() {
+    alert("Marcos Seghesio");
 }
-
