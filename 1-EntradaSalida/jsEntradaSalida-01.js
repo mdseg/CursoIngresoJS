c856b2b2933e792ac2477d87a2cf3c219c48b049
mdseg <marcos.seghesio@gmail.com>
2020-07-09 11:48:36 -0300

    Modificado 9-7

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -2,5 +2,5 @@
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar() {
-    alert("Marcos");
+    alert("Marcos Daniel Seghesio");
 }
